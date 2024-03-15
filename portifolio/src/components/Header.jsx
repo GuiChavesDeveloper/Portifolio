a9cc660879983cc99b74dd591ba9f42fce8eecc7
Guilherme Chaves <guilherme.chaves66@gmail.com>
2024-03-15 20:36:46 -0300

    Header 1.0

--- portifolio/src/components/Header.jsx
+++ portifolio/src/components/Header.jsx
@@ -1,6 +1,21 @@
+import SwitchButton from './SwitchButton'
+
+import '../styles/components/header.sass'
+
+import logo from "../assets/images/logo_image.png"
+
 function Header() {
     return (
         <>
+            <header>
+                <a href="#" className="logo"><img src={logo} alt="Logo Guilherme Chaves" className='logo' /></a>
+                <ul className='menu'>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Me</a></li>
+                    <li><a href="#">Portifolio</a></li>
+                </ul>
+                <SwitchButton />
+            </header>
         </>
     )
 }
---
+++ portifolio/src/components/SwitchButton.jsx
@@ -0,0 +1,21 @@
+import '../styles/components/switchbutton.sass'
+
+import sun_icon from "../assets/icon/sun_icon.png"
+
+function SwitchButton() {
+    const btn = document.querySelector('.btn')
+
+    return (
+        <>
+            <div className="btn">
+                <div className="btn_indicator">
+                    <div className="btn_icon-container">
+                        <img src={sun_icon} alt="sun icon" className="sun_icon" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SwitchButton
