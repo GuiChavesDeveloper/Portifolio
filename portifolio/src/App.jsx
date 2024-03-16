8bff06bcb48f5a3e1706052fa2ddd476eedd9d74
Guilherme Chaves <guilherme.chaves66@gmail.com>
2024-03-16 13:54:51 -0300

    Main part

--- portifolio/src/App.jsx
+++ portifolio/src/App.jsx
@@ -3,12 +3,29 @@ import MainContent from './components/MainContent'
 
 import './styles/components/app.sass'
 
+import instagram from './assets/icon/instagram_icon.png'
+import linkedin from './assets/icon/linkedin_icon.png'
+import github from './assets/icon/github_icon.png'
+import facebook from './assets/icon/facebook_icon.png'
+
 function App() {
 
   return (
     <>
-      <Header />
-      <MainContent />
+      <div className="app">
+        <Header />
+        <div className="main">
+          <h1>Guilherme Chaves</h1>
+          <h2>Full Stack Develover</h2>
+          <h3>Find Me Here:</h3>
+          <ul>
+            <li><a href="https://www.instagram.com/gs_chaves01/" target='_blank'><img src={instagram} alt="" /></a></li>
+            <li><a href="https://www.linkedin.com/in/guilherme-chaves-b2a691225/" target='_blank'><img src={linkedin} alt="" /></a></li>
+            <li><a href="https://github.com/GuiChavesDeveloper" target='_blank'><img src={github} alt="" /></a></li>
+            <li><a href="https://www.facebook.com/guilherme.chaves.7587370/" target='_blank'><img src={facebook} alt="" /></a></li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
--- portifolio/src/components/MainContent.jsx
+++ portifolio/src/components/MainContent.jsx
@@ -1,6 +1,10 @@
 function MainContent() {
     return (
         <>
+            <div className="main">
+                <h1>Guilherme Chaves</h1>
+                <h2>Full Stack Developer</h2>
+            </div>
         </>
     )
 }
